3547a4f79e43e80ec74982409e2ba8a540bf84e4
rspt <julien@rspt.ch>
2018-12-29 17:23:19 +0100

    Remove service worker registration

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', function(event) {
-  console.log('Service worker installing...');
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', function(event) {
-  console.log('Service worker activating...');
-});
